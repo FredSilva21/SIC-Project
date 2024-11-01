6cfe35e0f2aaaca89493e0bc657b7c81ad8eb3c3
FredSilva21 <fredsilva43@gmail.com>
2024-11-01 21:52:00 +0000

    Rating controller done

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -4,6 +4,7 @@ const Park = require("./park");
 const Place = require("./place");
 const Payment = require("./payment");
 const Report = require("./report");
+const ParkRating = require("./park_rating");
 
 User.hasMany(Place, { foreignKey: "id_user" });
 Place.belongsTo(User, { foreignKey: "id_user" });
@@ -20,12 +21,15 @@ Payment.belongsTo(User, { foreignKey: "id_user" });
 Park.hasMany(Report, { foreignKey: "id_park" });
 Report.belongsTo(Park, { foreignKey: "id_park" });
 
+ParkRating.belongsTo(Park, { foreignKey: "id_park" });
+ParkRating.belongsTo(User, { foreignKey: "id_user" });
 
 User.sync({ logging: false })
   .then(() => Park.sync({ logging: false }))
   .then(() => Place.sync({ logging: false }))
   .then(() => Report.sync({ logging: false }))
   .then(() => Payment.sync({ logging: false }))
+  .then(() => ParkRating.sync({ logging: false }))
   .then(() => {
     console.log("All tables created successfully.");
   })
@@ -38,4 +42,5 @@ module.exports = {
   Place,
   Payment,
   Report,
+  ParkRating
 };
