794a6eec41739851f7845c69b75a89508260b012
nunomeira11 <40220240@esmad.ipp.pt>
2024-11-04 12:37:42 +0000

    create navbar and card for park

---
+++ Frontend/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar">
+      <ul class="navbar-list">
+        <li class="navbar-item">
+          <router-link to="/"> <button>Home </button> </router-link>
+        </li>
+        <li class="navbar-item">
+            <router-link to="/about"> <button>Sobre Nós</button> </router-link>
+        </li>
+        <li class="navbar-item">
+            <router-link to="/about"> <button>Contacto</button> </router-link>
+        </li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      navigateTo(page) {
+        this.$router.push({ name: page });
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    background-color: #007BFF; /* Azul */
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+  }
+  
+  .navbar-list {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+  }
+  
+  .navbar-item button {
+    background: none;
+    border: none;
+    color: white;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+  
+  .navbar-item button:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
---
+++ Frontend/src/components/ParkingCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <router-link to="/park" class="parking-card">
+        <div class="parking-card">
+            <img src="../assets/img/parque1.jpeg" alt="Parque 1" class="parking-image" />
+            <div class="parking-info">
+                <h3 class="parking-name">Parque 1</h3>
+                <p class="parking-location">Rua Parque 1, Porto</p>
+                <p class="parking-price">Preço por Hora:  2.30 €</p>
+            </div>
+        </div>
+    </router-link>
+</template>
+  
+<script>
+    export default {
+        props: {
+            image: String,
+            name: String,
+            location: String,
+            pricePerHour: Number,
+        }
+    }
+</script>
+  
+<style scoped>
+  .parking-card {
+    display: flex;
+    flex-direction: column;
+    width: 250px; /* Formato retangular vertical */
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+  
+  .parking-image {
+    width: 100%;
+    height: 180px; /* Ajuste a altura conforme necessário */
+    object-fit: cover;
+  }
+  
+  .parking-info {
+    padding: 1rem;
+  }
+  
+  .parking-name {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin: 0.5rem 0;
+  }
+  
+  .parking-location,
+  .parking-price {
+    margin: 0.3rem 0;
+    color: #555;
+  }
+</style>
+  
