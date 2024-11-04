262f7dbf2a129b67fdb6470b288f743e224bde02
FredSilva21 <fredsilva43@gmail.com>
2024-11-04 11:52:51 +0000

    frontend structure

---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+
+  <div>
+    <h1>MyPark</h1>
+    <p>Welcome to MyPark, a place to share your favorite parks!</p>
+  </div>
+</template>
